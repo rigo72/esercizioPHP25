adbe7b39e8246466086e98b4e76ef38b8afff182
rigo72 <mattia.gori27@gmail.com>
2024-10-23 23:41:33 +0200

    Commit 2

--- operazioni.php
+++ operazioni.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <table border = "1">
+        <tr>
+            <th>Somma</th>
+            <th>Sottrazione</th>
+            <th>Moltiplicazione</th>
+            <th>Divisione</th>
+        </tr>
     <?php
         $n1 = $_GET["primo_numero"];
         $n2 = $_GET["secondo_numero"];
@@ -21,6 +28,18 @@
         }
         intval($n1);
         intval($n2);
+        $divisione;
+        if($n2 == 0){
+            $divisione = "Errore";
+        }else{
+            $divisione = $n1 / $n2;
+        }
+        $somma = $n1 + $n2;
+        $sottrazione = $n1 - $n2;
+        $moltiplicazione = $n1 * $n2;
+        echo "<th>{$somma}</th> <th>{$sottrazione}</th> <th>{$moltiplicazione}</th> <th>{$divisione}</th>";
     ?>
+        
+    </table>
 </body>
 </html>
